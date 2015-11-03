5c94f9751fd1c05e3446e1c92fe12ad1d1ab01b2
Robert Doroftei <robert@kokos.ro>
2015-11-03 12:59:15 +0200

    added github repo and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.kokos/scrap "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Extracting information from HTML resources"
+  :url "https://github.com/kokosro/scrap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
