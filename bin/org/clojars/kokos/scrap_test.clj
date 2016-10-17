ea6da7ae94815741ec0e3e1ab1403c180df14170
Robert Doroftei <robert@kokos.ro>
2016-10-18 01:16:20 +0300

    bin folder removed

--- bin/org/clojars/kokos/scrap_test.clj
+++
@@ -1,7 +0,0 @@
-(ns org.clojars.kokos.scrap-test
-  (:require [clojure.test :refer :all]
-            [org.clojars.kokos.scrap :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
