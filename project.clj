e6b780a9c7fcc19aca0027d89a5032344d97e531
Robert Doroftei <robert@kokos.ro>
2016-10-17 19:59:19 +0300

    bumped clojure to 1.8 and added dependencies for example.core

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject org.clojars.kokos/scrap "0.1.3"
+(defproject org.clojars.kokos/scrap "0.2.0"
   :description "Extracting information from HTML resources"
   :url "https://github.com/kokosro/scrap"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
                    :sign-releases false}]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.1.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.3.0"]
+                 [org.clojars.kokos/browser "0.1.0"]
                  [crouton "0.1.2"]
                  [uri "1.1.0"]
                  [clj-time "0.8.0"]
